939069fa7f4d29e11ba80fce775360bcd7facdc8
nicolsc <nicolas.lesconnec.pro@gmail.com>
2013-02-01 14:12:01 +0100

    Import results for final rounds

--- scripts/import_results.js
+++ scripts/import_results.js
@@ -1,37 +1,69 @@
 var _ = require('underscore'),
+	dateformat=require('dateformat'),
 	jsdom = require('jsdom');
 
-
+var now = new Date();
 console.log('### import results ###');
-console.log(new Date());
+console.log(now);
 
-var urls = ['http://www.lequipe.fr/Football/FootballResultatGroupe5448.html',
-			'http://www.lequipe.fr/Football/FootballResultatGroupe5449.html',
-			'http://www.lequipe.fr/Football/FootballResultatGroupe5450.html',
-			'http://www.lequipe.fr/Football/FootballResultatGroupe5451.html'];
+if (dateformat(now, 'yyyy-mm-dd')<'2013-01-31'){
+	getGamesFromWebPages(['http://www.lequipe.fr/Football/FootballResultatGroupe5448.html',
+				'http://www.lequipe.fr/Football/FootballResultatGroupe5449.html',
+				'http://www.lequipe.fr/Football/FootballResultatGroupe5450.html',
+				'http://www.lequipe.fr/Football/FootballResultatGroupe5451.html'],
+				{game:".content .ligne", home:'.equipeDom', away:'.equipeExt', score:'.score'},
+				importGames);
+}
+else{
+	/*
+	* Forget about importing data from the 'Tableau final', the score display is messy & would need some extra work
+	getGamesFromWebPages('http://www.lequipe.fr/Football/FootballTableau1881.html',
+				{game:'.itm', home:'.clubeq:first', away:'.clubeq:last', score:'.score_unique'},
+				importGames);
+	*/
+	getGamesFromWebPages(['http://www.lequipe.fr/Football/FootballResultatGroupe5452.html', //Quarter-finals
+				'http://www.lequipe.fr/Football/FootballResultatGroupe5453.html', //Semi finals
+				'http://www.lequipe.fr/Football/FootballResultat44318.html',//third place game
+				'http://www.lequipe.fr/Football/FootballResultat44317.html'], //Final
+				{game:".content .ligne", home:'.equipeDom', away:'.equipeExt', score:'.score'},
+				importGames);}
 
-var oneMoreRequest = _.after(urls.length, function cb(){importGames(games);});
-var games = [];
-urls.forEach(function(url){
-
-	jsdom.env({
-		html: url, //scrap that url
-		scripts: ["http://code.jquery.com/jquery.js"], //use jquery
-		done: function done(errors, window) {
-			window.$(".content .ligne").each(function() {
-				games.push({
-					team1:teams_matching[window.$('.equipeDom', this).text().trim()],
-					team2:teams_matching[window.$('.equipeExt', this).text().trim()],
-					score:window.$('.score', this).text().trim()
+/**
+* Get games from web
+* @param {Array} urls web pages to scrap
+* @param {Object} selectors
+* @param {String} selectors.game CSS selector to get each game
+* @param {String} selectors.home CSS selector to get home team name
+* @param {String} selectors.away CSS selector to get away team name
+* @param {String} selectors.score CSS selector to get score
+* @param {Function} callback called after scrapping each url
+**/
+function getGamesFromWebPages(urls, selectors,callback){
+	if (typeof urls == 'string'){
+		urls = [urls];
+	}
+	var oneMoreRequest = _.after(urls.length, function cb(){callback(games);});
+	var games = [];
+	urls.forEach(function(url){
+		jsdom.env({
+			html: url, //scrap that url
+			scripts: ["http://code.jquery.com/jquery.js"], //use jquery
+			done: function done(errors, window) {
+				window.$(selectors.game).each(function() {
+					games.push({
+						team1:teams_matching[window.$(selectors.home, this).text().trim()],
+						team2:teams_matching[window.$(selectors.away, this).text().trim()],
+						score:window.$(selectors.score, this).text().trim()
+					});
 				});
-			});
-			return oneMoreRequest();
-		}
+				return oneMoreRequest();
+			}
+		});
+
 	});
+}
 
-});
-
-var importGames = function importGames(games){
+function importGames(games){
 	var end = _.after(games.length, function(){process.exit(0);});
 	require('../config').getConfig(function(err, config) {
 		if (err) { console.log("ERROR", err); process.exit(1); }
@@ -65,9 +97,9 @@ var importGames = function importGames(games){
 			});
 		});
 	});
-};
+}
 
-var findGame = function (db, game, callback){
+function findGame(db, game, callback){
 
 	db.Fixture.find({team1:game.team1, team2:game.team2}, null, {}).exec(function(err, docs){
 		if (err){
@@ -100,15 +132,14 @@ var findGame = function (db, game, callback){
 					game.score += ' - '+tmpScore.join(' ');
 				}
 			}
-		}		
+		}
 
 		docs[0].score=game.score;
 		docs[0].save(function(err){
 			callback();
 		});
 	});
-};
-
+}
 
 
 var teams_matching = {
